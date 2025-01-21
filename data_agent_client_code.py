a332e295307a70f9c2e78d528ca88b7be0af2f9f
Jed Gore <jedgore@Jeds-MacBook-Pro.local>
2025-01-21 06:17:28 -0500

    pseudocode

--- data_agent_client_code.py
+++ data_agent_client_code.py
@@ -1,3 +1,11 @@
+from multiprocessing import Pool
+import requests
+import data_agent_post
+import pandas as pd
+import json
+from datetime import datetime as dt
+
+
 probabilities = {}
 truth = {}
 n_workers = 4
@@ -5,27 +13,25 @@ p = Pool(n_workers)
 
 
 base_url = "http://localhost:8000/get_svc?"
-for symbol in featureset["symbol"].unique()[2:]:  # AAPL is corrupted
+symbol_list = []
+jobs = []
+for symbol in symbol_list:
+    path = "worker_data/" + symbol + ".csv"
+    # df = featureset[featureset['symbol'] == symbol]
+    # dates = [y for y in df['date'] if dt.strptime(y, '%Y-%m-%d').year >= 2022]
+    # df.to_csv(path, index=False)
 
     print(symbol)
-    preds = []
-    truth[symbol] = []
-    path = "worker_data/" + symbol + ".csv"
-    df = featureset[featureset["symbol"] == symbol]
-    dates = [y for y in df["date"] if dt.strptime(y, "%Y-%m-%d").year >= 2022]
-
-    df.to_csv(path, index=False)
-    jobs = []
-    url = "file_path={}&predict_date={}&target_name=target"
-    for predict_date in dates:
-        truth[symbol].append(df[df["date"] == predict_date]["target"].iloc[0])
-        jobs.append(
-            {
-                "url": base_url + url.format(path, predict_date),
-                "data": ",".join(predictors),
-            }
-        )
-    results = p.map(post.wrech_mach, jobs)
+    some_variable = ""
+    data_list = []
+    url = ""
+    jobs.append(
+        {
+            "url": base_url + url.format(path, some_variable),
+            "data": ",".join(data_list),
+        }
+    )
+    results = p.map(data_agent_post.wrech_mach, jobs)
     probabilities[symbol] = [json.loads(r.content.decode("utf-8")) for r in results]
     break
 p.close()
